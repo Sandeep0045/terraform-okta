000170df6440dc68c3765f9ddfb1ca0f82792393
Sandeep <you@example.com>
2020-09-15 17:43:57 +0530

    adding user resources

--- main.tf
+++ main.tf
@@ -3,6 +3,15 @@ provider "okta" {
   base_url  = "okta.com"
   api_token = "00DOd-xVcpbabW_7mSE48RZyb6Jo94-1o1rmnJ_GYf"
 }
+
+resource "okta_user" "one" {
+  first_name = "demo"
+  last_name  = "terraform"
+  login      = "sandeep4597mac@gmail.com"
+  email      = "sandeep4597mac@gmail.com"
+  status     = "STAGED"
+}
+
 resource "okta_group" "example" {
   name        = "Example"
   description = "My Example Group"
