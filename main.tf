cf4fee37f0f2f922f8b73f24e0debcb507b9d39f
Sandeep0045 <38896678+Sandeep0045@users.noreply.github.com>
2020-09-16 22:44:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,15 +4,15 @@ provider "okta" {
   api_token = "00DOd-xVcpbabW_7mSE48RZyb6Jo94-1o1rmnJ_GYf"
 }
 
-resource "okta_user" "one" {
-  first_name = "pratik"
-  last_name  = "terraform"
-  login      = "sandeep4597mac@gmail.com"
-  email      = "sandeep4597mac@gmail.com"
+resource "okta_user" "one11" {
+  first_name = "pratik11"
+  last_name  = "terraform11"
+  login      = "sandeep459711mac@gmail.com"
+  email      = "sandeep4597mac11@gmail.com"
   status     = "STAGED"
 }
 
-resource "okta_group" "test" {
-  name        = "test"
-  description = "My test Group"
+resource "okta_group" "test11" {
+  name        = "test11"
+  description = "My test11 Group"
 }
